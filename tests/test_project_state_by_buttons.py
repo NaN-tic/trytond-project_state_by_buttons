9e6dbe27c8693de71913fba06824e03cf0210ac3
resteve <resteve@zikzakmedia.com>
2015-12-01 11:16:47 +0100

    Tests

--- tests/test_project_state_by_buttons.py
+++ tests/test_project_state_by_buttons.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part of the project_state_by_button module for Tryton.
+# This file is part of the project_state_by_buttons module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProjectStateByButtonsTestCase(unittest.TestCase):
+class ProjectStateByButtonsTestCase(ModuleTestCase):
     'Test Project State By Buttons module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('project_state_by_buttons')
-
-    def test0005views(self):
-        'Test views'
-        test_view('project_state_by_buttons')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'project_state_by_buttons'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ProjectStateByButtonsTestCase))
+        ProjectStateByButtonsTestCase))
     return suite
