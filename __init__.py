443a5e91c4dfae76c84cdfd714aea75bdf8be22b
Raimon Esteve <raimon@nan-tic.com>
2018-09-17 22:26:58 +0200

    Register classes

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .work import *
+from . import work
 
 
 def register():
     Pool.register(
-        Work,
+        work.Work,
         module='project_state_by_buttons', type_='model')
