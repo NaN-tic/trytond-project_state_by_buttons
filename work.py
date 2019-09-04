092560968fd0bd4818f239f9db6826615d877081
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2019-09-04 14:05:46 +0200

    Allow to desactivate projects and dont show the desactivate projects.

--- work.py
+++ work.py
@@ -1,15 +1,18 @@
 # This file is part of the project_state_by_button module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelView
+from trytond.model import ModelView, fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
+from trytond.transaction import Transaction
 
 __all__ = ['Work']
 
 
 class Work(metaclass=PoolMeta):
     __name__ = 'project.work'
+    active = fields.Function(fields.Boolean('Active'), 'get_active',
+        searcher='search_active')
 
     @classmethod
     def __setup__(cls):
@@ -33,3 +36,33 @@ class Work(metaclass=PoolMeta):
     @ModelView.button
     def done(cls, works):
         cls.write(works, {'state': 'done'})
+
+    @classmethod
+    def get_total(cls, works, names):
+        with Transaction().set_context(active_test=False):
+            return super().get_total(works, names)
+
+    def get_active(self, name):
+        if self.type == 'project':
+            return self.state == 'opened'
+        return True
+
+    @classmethod
+    def search_active(cls, name, clause):
+        operator = clause[1] == '=' and 1 or -1
+        operand = clause[2] and 1 or -1
+        sign = operator * operand
+
+        if sign > 0:
+            return ['OR', [
+                    ('type', '=', 'task')
+                    ], [
+                    ('type', '=', 'project'),
+                    ('state', '=', 'opened'),
+                    ]
+                ]
+        else:
+            return [
+                ('type', '=', 'project'),
+                ('state', '=', 'done'),
+                ]
